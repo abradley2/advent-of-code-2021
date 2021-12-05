02105ebde9bd3c89036af735fa14b2ca5b85ae81
Tony <anthony@wirewheel.io>
2021-12-05 08:05:07 -0500

    use sets

--- src/Solutions/Day4.hs
+++ src/Solutions/Day4.hs
@@ -1,45 +1,73 @@
-{-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE LambdaCase        #-}
 {-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TupleSections     #-}
 
 module Solutions.Day4 where
 
-import Data.IntMap.Lazy as IntMap (IntMap, insert)
-import Data.Foldable (toList)
-import Relude
-import Solutions.Evaluate (evaluate, Label(..), Solution)
-import Text.Parsec
-import qualified Text.Parsec as P ((<|>))
-import Text.Parsec.Text (Parser)
-import Text.Read (readMaybe)
-import Data.Sequence (mapWithIndex)
+import           Data.Foldable      (toList)
+import           Data.IntMap.Lazy   as IntMap (IntMap, insert)
+import           Data.Sequence      (mapWithIndex)
+import qualified Data.Sequence      as Seq (fromList)
+import           Data.Set.Ordered
+import qualified Data.Set.Ordered   as Set (fromList)
+import           Relude
+import           Solutions.Evaluate (Label (..), Solution, evaluate)
+import           Text.Parsec
+import qualified Text.Parsec        as P ((<|>))
+import           Text.Parsec.Text   (Parser)
+import           Text.Read          (readMaybe)
 
 type Entries = [Int]
 
 type Table = IntMap (IntMap (Int, Bool))
 
 partOneSampleSolution :: Solution
-partOneSampleSolution = evaluate inputParser partOne (Label "Part One Sample") "src/Solutions/Day4/sample_input.txt"
+partOneSampleSolution =
+  evaluate
+    inputParser
+    partOne
+    (Label "Part One Sample")
+    "src/Solutions/Day4/sample_input.txt"
 
 partOne :: (Entries, [Table]) -> Text
 partOne = show
 
+a :: IntMap (Int, Bool) -> IntMap (Int, Bool)
+a = fmap (\v -> v)
+
+checkTable :: Table -> Maybe Entries
+checkTable table = Nothing
+
+scoreTable :: Entries -> Table -> Maybe Text
+scoreTable (entry:next) table =
+  let updated =
+        fmap
+          (\(colVal, isChecked) ->
+             if colVal == entry
+               then (colVal, True)
+               else (colVal, isChecked)) <$>
+        table
+   in Nothing
+
+{- PARSING -}
 tableParser :: Int -> Table -> Parser Table
 tableParser rowIdx table = do
-  row <- toIntMap . toList . mapWithIndex (,) . fromList <$> rowParser
+  row <- toIntMap . toList . mapWithIndex (,) . Seq.fromList <$> rowParser
   let nextTable = IntMap.insert rowIdx row table
-  try (newline >> noneOf "\n" >> tableParser (rowIdx + 1) nextTable) P.<|> pure nextTable
+  try (newline >> noneOf "\n" >> tableParser (rowIdx + 1) nextTable) P.<|>
+    pure nextTable
   where
     toIntMap :: [(Int, a)] -> IntMap a
     toIntMap = fromList
-
     rowParser :: Parser [(Int, Bool)]
     rowParser = do
       void (many1 (char ' ')) P.<|> pure ()
-      val <- (many1 (noneOf " \n") >>= parseInt "Row value is not an int") <&> (,False)
+      val <-
+        (many1 (noneOf " \n") >>= parseInt "Row value is not an int") <&>
+        (, False)
       next <- (many1 (char ' ') >> rowParser) P.<|> mempty
-      pure (val:next)
+      pure (val : next)
 
 inputParser :: Parser (Entries, [Table])
 inputParser = do
@@ -57,7 +85,7 @@ inputParser = do
     tablesParser = do
       table <- tableParser 0 mempty
       next <- (newline >> newline >>= const tablesParser) P.<|> (eof >> pure [])
-      pure (table:next)
+      pure (table : next)
 
 parseInt :: [Char] -> [Char] -> ParsecT Text () Identity Int
 parseInt err = maybe (fail err) pure . readMaybe
